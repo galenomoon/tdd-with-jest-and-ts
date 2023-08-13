c54ebad5c2f1d68c3a040b071f1f719d988eb116
galenomoon <galeno.dev@gmail.com>
2023-08-13 12:25:08 -0300

    refacted code - using beforeAll to create a new instance for each test

--- src/tests/passwordChecker.spec.ts
+++ src/tests/passwordChecker.spec.ts
@@ -1,19 +1,29 @@
 import { PasswordChecker } from "../utils/passwordChecker"
 
 describe("PasswordChecker test suite", () => {
-  describe.only('Check if the password is NOT valid', () => {
-    const sut = new PasswordChecker()
+  let sut: PasswordChecker
 
-    const expectedErrors = [
-      { condition: '8 characters in your password', password: '1234567' },
-      { condition: '1 upper case letter in your password', password: 'apollo11' },
-      { condition: '1 lower case letter in your password', password: 'APOLLO11' },
-    ]
+  const cases = [
+    { condition: '8 characters in your password', password: '1234567' },
+    { condition: '1 upper case letter in your password', password: 'apollo11' },
+    { condition: '1 lower case letter in your password', password: 'APOLLO11' },
+  ]
 
-    it.each(expectedErrors)("Should throw error if you don't have at least $condition - password: $password", ({ condition, password }) => {
+  describe('Check if the password IS NOT VALID', () => {
+    beforeEach(() => { sut = new PasswordChecker() })
+    it.each(cases)("Should throw error if you don't have at least $condition", ({ condition, password }) => {
       const actual = () => sut.checkPassword(password)
       const expectedError = `You must have at least ${condition}`
       expect(actual).toThrowError(expectedError)
+    })
+  })
+
+  describe('Check if the password IS VALID', () => {
+    sut = new PasswordChecker()
+    it("Should pass if you have at least more than 8 characters, 1 upper case letter and 1 lower case letter in your password", () => {
+      const actual = sut.checkPassword('Apollo11')
+      const expected = "Your password is valid!"
+      expect(actual).toEqual(expected)
     })
   })
 })
--- src/utils/passwordChecker.ts
+++ src/utils/passwordChecker.ts
@@ -5,6 +5,6 @@ export class PasswordChecker {
     if (str.length < 8) throw new Error('You must have at least 8 characters in your password')
     if (!/[A-Z]/.test(str)) throw new Error('You must have at least 1 upper case letter in your password')
     if (!/[a-z]/.test(str)) throw new Error('You must have at least 1 lower case letter in your password')
-    return `Your password i:s ${str}`
+    return "Your password is valid!"
   }
 }
